9a3cfda97036f09b3d8e7f86d6d4bdd96a0ac6a4
iPrayag <prayag.upd@gmail.com>
2012-11-12 16:39:24 +0545

    add twittercontroller

---
+++ grails-app/controllers/whotweetsnepal/TwitterController.groovy
@@ -0,0 +1,16 @@
+package whotweetsnepal
+/*
+ * @author  : Prayag Upd
+ * @created : 12 Nov, 2012
+ */
+class TwitterController {
+
+    def index() { }
+    
+    /**
+      *render view twitter.gsp
+      */
+    def tw = {
+    render view: 'twitter'
+  }
+}
